697707b4edc4439bfe59c8b3481f52800f3985f8
Arthur Ramos <arthurramosm15@gmail.com>
2025-09-21 21:46:23 -0300

    Adicionar arquivos de projeto.

---
+++ APILista02/Controllers/ProdutoController.cs
@@ -0,0 +1,39 @@
+﻿using APILista02.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.RegularExpressions;
+
+namespace APILista02.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProdutoController : Controller
+    {
+
+        [HttpPost("cadastrar")]
+        public IActionResult CadastrarProduto([FromBody] Produto produto)
+        {
+            if (produto == null)
+                return BadRequest("Os dados do produto são obrigatórios.");
+
+            if (string.IsNullOrWhiteSpace(produto.Descricao))
+                return BadRequest("A descrição é obrigatória.");
+
+            if (produto.Preco <= 0)
+                return BadRequest("O preço deve ser maior que zero.");
+
+            if (produto.Estoque < 0)
+                return BadRequest("O estoque não pode ser negativo.");
+
+            if (string.IsNullOrWhiteSpace(produto.CodigoProduto))
+                return BadRequest("O código do produto é obrigatório.");
+
+            string padrao = @"^[A-Z]{3}-\d{4}$";
+
+            if (!Regex.IsMatch(produto.CodigoProduto, padrao))
+                return BadRequest("O código do produto deve estar no formato AAA-1234.");
+
+            return Ok(new { Mensagem = "Produto cadastrado com sucesso!", Produto = produto });
+        }
+
+    }
+}
---
+++ APILista02/Models/Produto.cs
@@ -0,0 +1,10 @@
+﻿namespace APILista02.Models
+{
+    public class Produto
+    {
+        public string Descricao { get; set; }
+        public double Preco { get; set; }
+        public int Estoque { get; set; }
+        public string CodigoProduto { get; set; }
+    }
+}
